ea9aa1256c29028d2de7099e84b62bb49a88aaf5
Sad-Wolverine <emanddemms@gmail.com>
2023-01-26 23:50:48 +0200

    add data types

---
+++ HW_1/main.js
@@ -0,0 +1,31 @@
+const number1 = 99999999999;
+const number2 = 99999999999n;
+console.log(number1);
+console.log(typeof number1);
+console.log(typeof number2);
+let string = 'Helo Word';
+console.log(string);
+console.log(typeof string);
+const str = 'value';
+console.log( `value${number1}`);
+let isGreater1 = 4 > 1;
+console.log(isGreater1);
+let isGreater2 = 2 > 3;
+console.log(isGreater2);
+console.log(typeof isGreater1);
+let age = null;
+console.log(age);
+console.log(typeof age);
+let number;
+console.log(number);
+console.log(typeof number);
+const user = {
+    name: 'John',
+    secondName: 'Doe'
+};
+console.log(typeof user, user);
+let fruits = ["apple", "cherry", "banana"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(typeof fruits);
